4ad15d88a68ef16e62609891651938e53f3b9b88
aliakseividaseu <alexaat@yahoo.co.uk>
2025-02-28 15:28:16 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const invitesRouter = require('./routes/invites')
+const friendsRouter = require('./routes/friends')
+const usersRouter = require('./routes/users')
+const locationsRouter = require('./routes/locations')
+const authRouter = require('./routes/auth')
+const {auth} = require('./middleware')
+const init = require("./db/init_db");
+var favicon = require('serve-favicon');
+
+const app = express();
+
+app.use(express.json())
+app.use(bodyParser.json());
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+init.createTables();
+
+app.get('/', (req, res) => {
+  init.createTables();
+  res.send('Welcome to kaquiz api');  
+});
+
+app.use(auth);
+
+app.use('/auth', authRouter)
+
+app.use('/invites', invitesRouter)
+app.use('/friends', friendsRouter)
+app.use('/users', usersRouter)
+app.use('/locations', locationsRouter)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
