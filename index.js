9647e0eedadd7977ecf3323fb6b4a486cefc37cb
aliakseividaseu <alexaat@yahoo.co.uk>
2025-03-03 23:24:54 +0000

    ws update

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const usersRouter = require('./routes/users')
 const locationsRouter = require('./routes/locations')
 const authRouter = require('./routes/auth')
 const {auth} = require('./middleware')
-const {init_ws} = require('./network/ws')
+const {init_ws, WS} = require('./network/ws')
 const init = require("./db/init_db");
 const favicon = require('serve-favicon');
 const app = express();
 const server = require('http').createServer(app);
-const ws = init_ws(server);
+
+const wss = init_ws(server);
+
 
 app.use(express.json())
 app.use(bodyParser.json());
@@ -21,9 +23,64 @@ init.createTables();
 
 app.get('/', (req, res) => {
   init.createTables();
-  res.send('Welcome to kaquiz api'); 
+  if (wss) {
+    wss.on('connection', (ws_socket) => {
+        ws_socket.send('New Message via websocket'); 
+    });   
+  }
+  //res.send('Welcome to kaquiz api'); 
+  res.send(
+    `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>WS TEST</title>   
+        </head>
+        <body>
+          <input type="submit" value="WS test" id="submit">
 
+          <script>
+              console.log('test js')
+              const socket = new WebSocket('ws://localhost:3000')
+              socket.addEventListener('open', e => {
+                  console.log('connection opened...');
+              });
+              socket.addEventListener('message', e => {
+                  console.log('incoming message: ' + e.data);
+              });
+              const btn = document.querySelector('input');
+              btn.addEventListener('click', () => {
+                 fetch("http://localhost:3000/", {
+                    method: "POST",
+                    body: JSON.stringify({}),
+                    headers: {
+                      "Content-type": "application/json; charset=UTF-8"
+                    }
+                  })
+                    .then((response) => response.json())
+                    .then((json) => console.log(json));
+              })
+
+          </script>
+        </body>
+      </html>
+    `
+  ); 
 });
+
+app.post('/', (req, res) => {
+  console.log("wss "+ wss);
+  if (wss) {
+    wss.clients.forEach(function each(client) {
+      client.send('POST request. New Message via websocket');
+   });
+
+  }
+  res.status(200).send('{"status": "OK"}');
+})
 
 app.use(auth);
 
--- network/ws.js
+++ network/ws.js
@@ -1,14 +1,13 @@
 const { WebSocketServer } = require('ws');
 
-let WS;
+
 
 const init_ws = (server)  => {
     const wss = new WebSocketServer({server});
 
     wss.on('connection', function connection(ws) {       
         ws.on('error', console.error);
-        ws.send('ws connection seccessful...');
-        WS = wss;
+        ws.send('ws connection seccessful...');       
       });
 
     return wss;
@@ -16,6 +15,6 @@ const init_ws = (server)  => {
 
 module.exports = {
     init_ws,
-    WS
+    
 }
 
